2fd38e07086686e9e1ee40c4c1795e6d8474b7b2
R.Soler <62470437+RicaReloS@users.noreply.github.com>
2024-12-20 13:49:32 +0100

    actualizacion git

--- wsgi_app.py
+++ wsgi_app.py
@@ -1,11 +1,22 @@
 # gunicorn
 
+# APP_MODE="local"
+# APP_MODE="production"
 
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv(".env")
 
 def app(environ, start_response):
     status = "200 OK"
     headers = [
         ("Content-Type", "text/plain; charset=utf-8")
     ]
+    app_mode=os.environ.get("APP_MODE")
+    print(app_mode)
+    secret=os.environ.get("SECRET")
+    print(secret)
     start_response(status, headers)
     return [b"Hello, WSGI World"]
